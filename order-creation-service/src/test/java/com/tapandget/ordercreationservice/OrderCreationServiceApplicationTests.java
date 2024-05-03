c1aaedb0ac08efac20aca2a45cf49c22075dd31e
marco.marani <marco.marani@hotmail.it>
2024-05-03 16:16:21 +0200

    Add modules

---
+++ order-creation-service/src/test/java/com/tapandget/ordercreationservice/OrderCreationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapandget.ordercreationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCreationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ warehouse-management-service/src/main/java/com/tapandget/warehousemanagementservice/WarehouseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tapandget.warehousemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementServiceApplication.class, args);
+	}
+
+}
