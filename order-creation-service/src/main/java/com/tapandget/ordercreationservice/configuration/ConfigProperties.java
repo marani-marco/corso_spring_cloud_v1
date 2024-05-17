8f34762638b2fe31fd604e2b810137873a5361f2
marco.marani <marco.marani@hotmail.it>
2024-05-17 14:58:14 +0200

    Add configurations for order-creation-service, add connection to spring cloud config server from order-creation-service

--- order-creation-service/src/main/java/com/tapandget/ordercreationservice/configuration/ConfigProperties.java
+++ order-creation-service/src/main/java/com/tapandget/ordercreationservice/configuration/ConfigProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "order-creation-servicetap")
+@ConfigurationProperties(prefix = "order-creation-service")
 public class ConfigProperties {
 
     private String maximumTimeCreation;
--- order-creation-service/src/main/java/com/tapandget/ordercreationservice/controller/OrderCreationController.java
+++ order-creation-service/src/main/java/com/tapandget/ordercreationservice/controller/OrderCreationController.java
@@ -1,12 +1,10 @@
 package com.tapandget.ordercreationservice.controller;
 
+import com.tapandget.ordercreationservice.configuration.ConfigProperties;
 import com.tapandget.ordercreationservice.entity.Order;
 import com.tapandget.ordercreationservice.repository.OrderCreationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order-creation")
@@ -15,9 +13,17 @@ public class OrderCreationController {
     @Autowired
     OrderCreationRepository orderCreationRepository;
 
+    @Autowired
+    ConfigProperties configProperties;
+
     @PostMapping
     public void saveOrder(@RequestBody Order order){
         orderCreationRepository.save(order);
+    }
+
+    @GetMapping
+    public String orarioMaxCreazione(){
+        return configProperties.getMaximumTimeCreation();
     }
 
 }
